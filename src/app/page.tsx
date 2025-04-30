6a811cad0d9660299e8f8ae04ed2023537bb9c8d
Pradipta Das <dipta12@gmail.com>
2025-04-30 20:43:36 +0530

    second commit 30-04-25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,6 @@ export default function Page() {
       <EmploymentSection/>     
       <PortfolioSection />
       <ExperienceSection />  
-      
       <ContactSection />
       
     </section>
