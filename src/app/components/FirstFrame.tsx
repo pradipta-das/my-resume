9d1e73745b57fac75ac6774690c7089d960bdbaa
Pradipta Das <dipta12@gmail.com>
2025-07-12 00:50:57 +0530

    Clean up

--- src/app/components/FirstFrame.tsx
+++ src/app/components/FirstFrame.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React,{useRef} from 'react';
+import React from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
