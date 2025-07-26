e71d19d141222b95f89436533344199ccc5ef479
Pradipta Das <dipta12@gmail.com>
2025-07-26 15:17:36 +0530

    prod level issue fixed

--- src/app/components/FirstFrame.tsx
+++ src/app/components/FirstFrame.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import gsap from 'gsap';
-import { useRef, useEffect } from 'react';
 import { useGSAP } from '@gsap/react';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import ScrollSmoother from 'gsap/ScrollSmoother';
